ebf2491d430d5b9dcb501f8bc785f497957b0173
Luke <luke@Lukes-MacBook-Pro.local>
2018-10-11 15:42:18 -0700

    created VideoDislike model

--- vid_corner_app/models.py
+++ vid_corner_app/models.py
@@ -44,5 +44,13 @@ class VideoLike(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class VideoDislike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    video = models.ForeignKey(Video_Upload, on_delete=models.CASCADE, related_name='video_upload')
+
+    def __str__(self):
+        return self.user.username
     
     
